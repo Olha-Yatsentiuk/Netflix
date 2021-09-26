3211fa695d3d32ee548fbbcbc2455effcb5b908e
Olha Yatsentiuk <Olha_Yatsentiuk@epam.com>
2021-09-26 18:45:07 +0300

    added firebase, started form

---
+++ src/lib/firebase.prod.js
@@ -0,0 +1,19 @@
+import Firebase from 'firebase/compat/app'; 
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+//import { seedDatabase } from './seed';
+
+const config = {
+  apiKey: "AIzaSyDPc25-7VByFbaWcmRm45HxAs--NWOiRR8",
+  authDomain: "copyofnetflix.firebaseapp.com",
+  projectId: "copyofnetflix",
+  storageBucket: "copyofnetflix.appspot.com",
+  messagingSenderId: "779034482992",
+  appId: "1:779034482992:web:9d4682829f41bb4e8e90f5"
+};
+
+const firebase = Firebase.initializeApp(config);
+//seedDatabase(firebase);
+
+
+export { firebase };
